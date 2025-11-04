7a960f78e37ed59590094faecda36933fb91f9e1
anaswara11111-lang <anaswara11111@gmail.com>
2025-11-04 14:55:07 +0530

    Add files via upload

---
+++ stackk.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#define max_size 10
+int s[max_size],top=-1;
+void push(int item)
+{
+	if(top>=max_size-1)
+	{
+		printf("Stack is full");
+	}
+	else
+	{
+		printf("Enter the element to push:");
+		scanf("%d",&item);
+		top=top+1;
+		s[top]=item;
+	}
+}
+void pop()
+{
+	if(top==-1)
+	{
+		printf("Stack is empty");
+	}
+	else
+	{
+		printf("pop element is %d",s[top]);
+		top=top-1;
+	}
+}
+void display()
+{ 
+        int i;
+	for(i=top;i>-1;i--)
+	{
+		printf("%d ",s[i]);
+	}
+}
+
+int main()
+{
+	int choice,item;
+	do
+	{
+		printf("\n1.push \n2.pop \n3.display \n4.exit\n");
+		printf("Enter your choice:");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1: push(item);
+			        break;
+			case 2: pop();
+			        break;
+			case 3: display(); 
+			        break;
+			case 4: break;
+			default: printf("Your choice is invalid");
+				 break;
+        	}
+        }while(choice!=4);
+return(0);
+} 
